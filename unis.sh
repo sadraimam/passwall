40bf5d77e8ea3d7ea0c4a85905ed354c6484905c
sadraimam <sadra.imam@gmail.com>
2025-07-27 10:27:34 +0330

    Add files via upload

---
+++ unis.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sleep 3
+
+cd /root/
+
+echo "Unistalling ..."
+
+rm owo.sh
+
+rm up.sh
+
+rm timer.sh
+
+rm amirhossein.sh
+
+rm /etc/init.d/amir
+
+rm /sbin/amir
+
+cd /root/
+
+echo "Done !"
+
+sleep 5
+
+rm unis.sh
