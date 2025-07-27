c292abfedfc51ce2e24e580c0c409e0aac8f25b2
sadraimam <sadra.imam@gmail.com>
2025-07-27 10:41:25 +0330

    Update mahsa-s.sh

--- mahsa-s.sh
+++ mahsa-s.sh
@@ -150,7 +150,7 @@ echo -e "${GREEN} Xray : OK ${NC}"
 
  else
            
-rm -f amirhossein.sh && wget https://raw.githubusercontent.com/amirhosseinchoghaei/mi4agigabit/main/amirhossein.sh && chmod 777 amirhossein.sh && sh amirhossein.sh
+rm -f pw.sh && wget https://raw.githubusercontent.com/sadraimam/passwall/main/pw.sh && chmod 777 pw.sh && sh pw.sh
 
 fi
 
